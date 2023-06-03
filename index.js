9eb266c017184f5ecac7ae68fa466397f1b70fee
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-03 08:26:12 +0600

    cards delete api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -57,6 +57,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/cards/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cardsCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
