cf131f816b2fbeeb34c5ca3bdc8e3d1609c0984c
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-06 13:03:17 +0600

    admin verify function added

--- index.js
+++ index.js
@@ -60,9 +60,25 @@ async function run() {
             res.send({ token })
         })
 
+        // warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+            next();
+        }
+
+        /**
+         * 0. do not show secure links to those who should not see the links
+         * 1. use jwt token: verifyToken
+         * 2.use verifyAdmin middleware
+         * **/
 
         // users related apis
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
