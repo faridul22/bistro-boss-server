6bf83046ac1e8b8b7288c4e386829b105b020434
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-06 10:38:28 +0600

    verifyJWT function added

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+
+}
+
 // ---------------------
 
 
@@ -87,11 +105,17 @@ async function run() {
         });
 
         // card collection apis
-        app.get('/cards', async (req, res) => {
+        app.get('/cards', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
             }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
             const query = { email: email };
             const result = await cardsCollection.find(query).toArray();
             res.send(result)
